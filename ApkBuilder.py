ebbc9406cec9a12ffa7f3d52a8829653ae7c441a
adityaup22 <aditya@sportsunity.co>
2018-03-15 15:42:45 +0530

    Writing File

--- ApkBuilder.py
+++ ApkBuilder.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from commands import getoutput
 import os
 
@@ -11,6 +11,37 @@ def run_command():
     cmd = 'sudo ./gradlew assembleDevDebug'
     output = getoutput(cmd)
     return output
+
+
+@app.route('/utm_apk')
+def generate_utm_specific_apk():
+    CAMPAIGN = request.args.get('CAMPAIGN')
+    SOURCE = request.args.get('SOURCE')
+    MEDIUM = request.args.get('MEDIUM')
+    TERM = request.args.get('TERM')
+
+    data = """
+        package com.sports.unity.login.model;
+
+    /**
+     * Created by aditya on 15/03/18.
+     */
+
+    public class UtmConstants {
+        public static final String CAMPAIGN = "{}";
+        public static final String SOURCE = "{}";
+        public static final String MEDIUM = "{}";
+        public static final String TERM = "{}";
+    }
+
+        """.format(CAMPAIGN, SOURCE, MEDIUM, TERM)
+    f = open('/home/sportsunity/SportsUnityAndroid/app/src/main/java/com/sports/unity/login/model/UtmConstants.java')
+    f.writelines(data)
+    f.close()
+
+
+def write_file():
+    pass
 
 
 if __name__ == '__main__':
