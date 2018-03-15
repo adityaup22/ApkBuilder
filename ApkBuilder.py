a4612fab5688aeac34906995498dfe373611323a
adityaup22 <aditya@sportsunity.co>
2018-03-15 13:43:35 +0530

    Testing running build Commands

--- ApkBuilder.py
+++ ApkBuilder.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from commands import getoutput
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello APkBuilder!'
+def run_command():
+    os.chdir('/home/sportsunity/SportsUnityAndroid')
+    cmd = 'sudo ./gradlew assembleDevDebug'
+    output = getoutput(cmd)
+    return output
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
